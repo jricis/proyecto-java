082d1b58029d242d0a0a8cd0d07bc804f46c5937
Jonathan Ricis <jricis19@ilg.cat>
2021-01-22 13:33:33 +0100

    base de datos y relaciones

--- pryecto/api/models.py
+++ pryecto/api/models.py
@@ -7,35 +7,38 @@ import random
 
 
 class Rider(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4,editable=False)
-    type_vehicle = models.CharField
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    type_vehicle = models.CharField(max_length = 45,default="")
 
 class Image(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4,editable=False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     url = models.URLField(max_length = 200)
 
 class Restaurant(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4,editable=False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=30)
     adress = models.CharField(max_length=50, unique=True)
-    phone = models.IntegerField
-    description = models.TextField
+    phone = models.IntegerField(null=False,default=0)
+    description = models.TextField(blank=True)
     preparado  = models.BooleanField(default=False)
     riders = models.ManyToManyField(Rider)
     image_idImage = models.ForeignKey(Image, on_delete=models.CASCADE)
 
 class User(models.Model):
     objects = None
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4,editable=False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.CharField(max_length=20)
+    name = models.CharField(max_length=20,default="")
     surname = models.CharField(max_length=50, null=True)
     email = models.EmailField(max_length=45,unique=True)
-    phone = models.IntegerField
-    birthday = models.DateField
+    postalCode = models.PositiveIntegerField(default=00000)
+    city = models.CharField(max_length=50, default="")
+    phone = models.IntegerField(null=False,default=0)
+    birthday = models.DateField(null=True)
     password = models.CharField(max_length=45)
     created_at = models.DateTimeField(auto_now_add=True)
-    rider_idRider = models.ForeignKey(Rider, models.CASCADE)
-    restaurant_idRestaurant = models.ForeignKey(Restaurant, models.CASCADE)
+    Rider = models.ForeignKey(Rider, on_delete=models.CASCADE, blank=True, null=True)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE, blank=True, null=True)
 
 
 
@@ -43,25 +46,24 @@ class Pedido(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4,editable=False)
     name = models.CharField(max_length=45,default="")
     price = models.DecimalField(max_digits=5, decimal_places=2)
-    description = models.TextField
-    image_idImage = models.ForeignKey(Image, on_delete=models.CASCADE)
+    description = models.TextField()
+    image_idImage = models.ForeignKey(Image, on_delete=models.CASCADE, blank=True, null=True)
     restaurants = models.ManyToManyField(Restaurant)
 
 class Order(models.Model):
+    class EligeEstado(models.TextChoices):
+        PREPARADO = 'preparado'
+        ENTREGADO = 'entregado'
+        COCINANDO = 'en concina'
+        RECIBIDO = 'recibida la solicitud de pedido'
+
     id = models.UUIDField(primary_key=True, default=uuid.uuid4,editable=False)
-    num_order = models.PositiveIntegerField
+    num_order = models.PositiveIntegerField()
     total_price = models.DecimalField(max_digits=5, decimal_places=2)
     entregado = models.BooleanField(default=False)
-    estado = models.CharField(max_length=45,default="")
+    estado = models.CharField(max_length=45, choices= EligeEstado.choices, default=EligeEstado.RECIBIDO)
     pagado = models.BooleanField(default=False)
 
+    
 
 
-# class Restaurant_has_Rider(models.Model):
-#     Restaurant_idRestaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
-#     Rider_idRider = models.ForeignKey(Rider, on_delete=models.CASCADE)
-
-# class Restaurant_has_Pedido(models.Model):
-#     Restaurant_idRestaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
-#     Pedido_idOrder = models.ForeignKey(Pedido, on_delete=models.CASCADE)
-#     Restaurant_Image_idImg = models.ForeignKey(Image, on_delete=models.CASCADE)
